ea7583e66b6d2a7f05591166fb4c6e9487dfe805
Hendrik Mennen <hmennen@protop-solutions.com>
2024-02-06 20:02:27 +0100

    .

--- src/pages/plc-one.tsx
+++ src/pages/plc-one.tsx
@@ -13,10 +13,10 @@ export default function Studio(): JSX.Element {
 
       <header>
         <div className="flex">
-          <div className="flex justify-center mx-auto w-screen h-screen">
+          <div className="flex justify-center mx-auto h-screen w-screen">
             <img
               src={"/img/plc2/Schaltschrank.png"}
-              className="object-cover"
+              className="object-cover w-screen"
               alt="PLC Logo"
             />
             <div className="absolute flex flex-col justify-center w-full h-full">
@@ -44,7 +44,7 @@ export default function Studio(): JSX.Element {
             className="md:p-5 flex-col flex gap-1 justify-center relative"
           >
             <span className="text-3xl text-center md:text-left">
-              PLC ONE is the first Industry 5.0 ready industrial controller that can adapt to the needs of the future
+              PLC ONE is the first <span className="font-bold primary-text">Industry 5.0</span> ready industrial controller that can adapt to the needs of the <span className="font-bold primary-text">future</span>
             </span>
           </div>
         </div>
@@ -56,7 +56,7 @@ export default function Studio(): JSX.Element {
             AI-Powered Industrial Controller
             </h2>
             <img src={"/img/plc2/Drohne.png"} className="mt-10" alt="Compare" />
-            <div className="text-xl mt-12">
+            <div className="text-xl mt-6">
             Elevate your drone capabilities with our unified controller managing both motors and cameras. AI object recognition assists in navigation, combining the functions of processors, graphics cards, and drone controls into one lightweight, energy-efficient unit. Discover how our innovative approach can enhance your drone's performance and efficiency.
             </div>
           </div>
@@ -67,7 +67,7 @@ export default function Studio(): JSX.Element {
               One Simple Solution for the Tasks of the Future
             </h2>
             <img src={"/img/plc2/Steuerung.png"} className="mt-10" alt="Compare" />
-            <div className="text-xl mt-12">
+            <div className="text-xl mt-6">
             Discover the next generation of industrial automation: our controller features a new processor architecture for markedly faster response times than conventional PLCs. It centralizes processing to enhance energy efficiency, minimize weight, and reduce costs. With its capacity for unlimited expansion, it’s built to accommodate extra computing power and components as needed. Upgrade and future-proof your electronics with our next-level control technology.
             </div>
           </div>
