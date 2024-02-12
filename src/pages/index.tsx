ed1fbc8fffc782dedbbfe0bfdba74e32065a6b10
Hendrik Mennen <hmennen@protop-solutions.com>
2024-02-12 18:57:00 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -324,7 +324,7 @@ export default function Home(): JSX.Element {
     >
       <HomepageHeader />
       <main className="container overflow-x-hidden md:overflow-x-visible">
-        <div className="my-20 flex flex-col justify-center align-middle">
+        <div className="my-20 flex flex-col justify-center align-middle" data-aos="fade-up">
           <h1 className="text-5xl text-center font-medium">Get ready for the next <span className="font-bold primary-text">Industrial Revolution</span></h1>
           <img src="/img/start/Overview.png" className="mx-auto mt-10" width={900}/>
         </div>
