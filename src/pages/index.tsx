2c9dc18f1e30fe5d93c1fd1379e9d6aab62ce941
Hendrik Mennen <hmennen@protop-solutions.com>
2024-02-18 11:47:36 +0100

    fx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -323,7 +323,7 @@ export default function Home(): JSX.Element {
       description="Empowering Industry 5.0"
     >
       <HomepageHeader />
-      <main className="container overflow-x-hidden md:overflow-x-visible">
+      <main className="container overflow-x-hidden">
         <div className="my-20 flex flex-col justify-center align-middle" data-aos="fade-up">
           <h1 className="text-5xl text-center font-medium">Get ready for the next <span className="font-bold primary-text">Industrial Revolution</span></h1>
           <img src="/img/start/Overview.png" className="mx-auto mt-10" width={900}/>
