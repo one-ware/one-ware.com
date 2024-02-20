4fdc687199e6257cfcfc61acb8ce533b887797b3
Hendrik Mennen <hmennen@protop-solutions.com>
2024-02-20 19:35:39 +0100

    Update studio.tsx

--- src/pages/studio.tsx
+++ src/pages/studio.tsx
@@ -10,7 +10,7 @@ import ContactUs from "../components/ContactUs";
 
 const sliders = [
   {
-    title: "Ai Generator",
+    title: "AI Generator",
     imageSrc: <img alt="Extensible" src="/img/studio/slides/ai.png" />,
     description: (
       <>
