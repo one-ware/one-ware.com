e77453c3b2b46a57f9c5ec89916497ef49880ea5
Hendrik Mennen <hmennen@protop-solutions.com>
2024-02-23 14:40:32 +0100

    .

--- src/pages/plc-one.tsx
+++ src/pages/plc-one.tsx
@@ -106,7 +106,7 @@ export default function PLC(): JSX.Element {
               )}
             </div>
             <div className="absolute flex flex-col xl:flex-row w-full container gap-10 md:gap-0">
-              <div className="xl:w-1/2 w-full flex flex-col justify-center gap-5 md:gap-10 mt-48 xl:mt-0">
+              <div className="xl:w-1/2 w-full flex flex-col justify-center gap-5 md:gap-10 md:mt-48 xl:mt-0">
                 <img
                   src={"/img/plc/Logo_SVG-ONE2-plc.svg"}
                   alt="PLC Logo"
