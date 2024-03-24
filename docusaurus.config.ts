71dafaa0a8a782b91cf4dfa1953b72ecd572b1f5
Hendrik Mennen <hmennen@protop-solutions.com>
2024-03-24 21:17:17 +0100

    .

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -77,33 +77,10 @@ const config: Config = {
 
   themeConfig: {
     algolia: {
-      // The application ID provided by Algolia
       appId: 'GVARI8M72O',
-
-      // Public API key: it is safe to commit it
       apiKey: '7d1be6ae06ba5fb5e97018bc2daab94a',
-
       indexName: 'one-ware',
-
-      // Optional: see doc section below
-      contextualSearch: true,
-
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      //externalUrlRegex: 'external\\.com|domain\\.com',
-
-      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      //replaceSearchResultPathname: {
-      //  from: '/docs/', // or as RegExp: /\/docs\//
-      //  to: '/',
-      //},
-
-      // Optional: Algolia search parameters
-      searchParameters: {},
-
-      // Optional: path for search page that enabled by default (`false` to disable it)
       searchPagePath: 'search',
-
-      //... other Algolia params
     },
     colorMode: {
       defaultMode: "dark",
