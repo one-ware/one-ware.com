65da5d8578a06d8752876e3ef2dec8d5e6b50e13
Hendrik Mennen <hmennen@protop-solutions.com>
2024-07-25 14:49:08 +0200

    update styles & remove fireworks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,6 @@ import initWebsiteEffects from "../components/startEffects";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   const [init, setInit] = useState(false);
-  const [enableFireworks, setEnableFireworks] = useState(true);
-
-  let fireworkContainer: Container | undefined = undefined;
 
   useEffect(() => {
     initParticlesEngine(async (engine) => {
@@ -27,22 +24,6 @@ function HomepageHeader() {
       setInit(true);
     });
   }, []);
-
-  const particlesLoaded = async (container?: Container | any) => {
-  };
-
-  const particlesLoadedStop = async (container?: Container | any) => {
-    var c = container as Container;
-    if (c != null) {
-      c.stop();
-      fireworkContainer = c;
-    }
-  };
-
-  const activateFirework = () => {
-    //fireworkContainer?.stop();
-    //fireworkContainer?.start();
-  };
 
   return (
     <header
@@ -53,7 +34,6 @@ function HomepageHeader() {
         {init && (
           <Particles
             id="stars"
-            particlesLoaded={particlesLoaded}
             className="h-full w-full absolute"
             options={{
               fullScreen: {
@@ -73,7 +53,7 @@ function HomepageHeader() {
                   },
                   modes: {
                     push: {
-                      quantity: 4,
+                      quantity: 1,
                     },
                     bubble: {
                       size: 6,
@@ -126,177 +106,11 @@ function HomepageHeader() {
             }}
           />
         )}
-
-        {init && (
-          <Particles
-            id="fireworks"
-            particlesLoaded={particlesLoadedStop}
-            className="h-full w-full absolute"
-            options={{
-              fullScreen: {
-                enable: false,
-              },
-              detectRetina: true,
-              fpsLimit: 144,
-              emitters: {
-                direction: "top",
-                life: {
-                  count: 10,
-                  duration: 0.1,
-                  delay: 0.3,
-                },
-                rate: {
-                  delay: 0.1,
-                  quantity: 1,
-                },
-                size: {
-                  width: 100,
-                  height: 0,
-                },
-                position: {
-                  y: 100,
-                  x: 50,
-                },
-              },
-              particles: {
-                number: {
-                  value: 0,
-                },
-                destroy: {
-                  mode: "split",
-                  bounds: {
-                    top: 10,
-                  },
-                  split: {
-                    count: 1,
-                    factor: { value: 3 / 3 },
-                    rate: {
-                      value: 50,
-                    },
-                    particles: {
-                      color: {
-                        value: [
-                          "#5bc0eb",
-                          "#fde74c",
-                          "#9bc53d",
-                          "#e55934",
-                          "#fa7921",
-                        ],
-                      },
-                      stroke: {
-                        width: 0,
-                      },
-                      number: {
-                        value: 0,
-                      },
-                      opacity: {
-                        value: { min: 0.1, max: 1 },
-                        animation: {
-                          enable: true,
-                          speed: 0.2,
-                          sync: false,
-                          startValue: "max",
-                          destroy: "min",
-                        },
-                      },
-                      shape: {
-                        type: "circle",
-                      },
-                      size: {
-                        value: { min: 2, max: 3 },
-                      },
-                      /*life: {
-                            count: 1,
-                            duration: {
-                              value: {
-                                min: 1,
-                                max: 2
-                              }
-                            }
-                          },*/
-                      move: {
-                        enable: true,
-                        gravity: {
-                          enable: false,
-                        },
-                        speed: { min: 5, max: 10 },
-                        direction: "outside",
-                        random: true,
-                        outMode: "destroy",
-                      },
-                    },
-                  },
-                },
-                life: {
-                  count: 1,
-                },
-                shape: {
-                  type: "line",
-                  fill: true,
-                },
-                size: {
-                  value: { min: 20, max: 50 },
-                  animation: {
-                    enable: true,
-                    sync: true,
-                    speed: 70,
-                    startValue: "random",
-                    destroy: "min",
-                  },
-                },
-                stroke: {
-                  color: {
-                    value: "#ffffff",
-                  },
-                  width: 1,
-                },
-                rotate: {
-                  path: true,
-                },
-                move: {
-                  enable: true,
-                  gravity: {
-                    acceleration: 15,
-                    enable: true,
-                    inverse: true,
-                    maxSpeed: 100,
-                  },
-                  speed: { min: 10, max: 20 },
-                  outModes: {
-                    default: "destroy",
-                    top: "none",
-                  },
-                },
-              },
-            }}
-          />
-        )}
-      </div>
-
-      <div className="w-full absolute h-screen hidden">
-        <div className="container mx-auto h-full">
-          <div className={`absolute top-50 right-28 hidden md:block ${styles.drone}`}>
-            <img
-              src={"img/start/drone_transparent.png"}
-              alt={""}
-              width={220}
-            />
-          </div>
-
-          <div className="absolute bottom-20 left-15 hidden md:block">
-            <img
-              src={"img/start/roboter_transparent.png"}
-              alt={""}
-              height={110}
-            />
-          </div>
-        </div>
       </div>
 
       <div className="absolute flex flex-col items-center justify-center w-full h-full pointer-events-none">
         <div className="flex flex-col items-center justify-center w-full md:w-6/12 ">
           <img
-            onClick={() => activateFirework()}
             className="drop-shadow-4xl"
             src="img/start/Logo_SVG-ONE-ware.svg"
             alt="OneWare Logo"
--- src/pages/studio.tsx
+++ src/pages/studio.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./studio.module.css";
 import "aos/dist/aos.css";
 import classnames from "classnames";
@@ -7,19 +7,23 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import initWebsiteEffects from "../components/startEffects";
 import ContactUs from "../components/ContactUs";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import { loadFull } from "tsparticles";
 
 const sliders = [
   {
     title: "Code Assistant",
-    imageSrc: <img alt="Code Assistant" src="/img/studio/slides2/completion.png" />,
+    imageSrc: (
+      <img alt="Code Assistant" src="/img/studio/slides2/completion.png" />
+    ),
     description: (
       <>
-        First class support for <span className="primary-text">C++</span>,{" "} 
-        <span className="primary-text">Python</span>,{" "} 
+        First class support for <span className="primary-text">C++</span>,{" "}
+        <span className="primary-text">Python</span>,{" "}
         <span className="primary-text">VHDL</span>,{" "}
-        <span className="primary-text">Verilog</span>{" "}and{" "}
-        more with lots of tools to help
-        develop your designs as efficiently as possible.
+        <span className="primary-text">Verilog</span> and more with lots of
+        tools to help develop your designs as efficiently as possible.
       </>
     ),
   },
@@ -29,7 +33,8 @@ const sliders = [
     description: (
       <>
         Conveniently setup and compile your designs from your hardware.{" "}
-        <span className="primary-text">Directy</span> from the IDE, which a growing number of supported toolchains and boards.
+        <span className="primary-text">Directy</span> from the IDE, which a
+        growing number of supported toolchains and boards.
       </>
     ),
   },
@@ -38,7 +43,8 @@ const sliders = [
     imageSrc: <img alt="Simulation" src="/img/studio/slides2/simulator.png" />,
     description: (
       <>
-        Simulate your designs inside the IDE, using the lightning fast Simulation Viewer and the most popular{" "}
+        Simulate your designs inside the IDE, using the lightning fast
+        Simulation Viewer and the most popular{" "}
         <span className="primary-text">Simulation</span> tools.
       </>
     ),
@@ -49,12 +55,150 @@ const sliders = [
     description: (
       <>
         Download and Install <span className="primary-text">Extensions</span>{" "}
-        from to extend the functionality. Discover a growing selection
-        of <span className="primary-text">AI-Tools</span>, <span className="primary-text">Hardware-Integrations</span>, <span className="primary-text">Simulators and More</span>
+        from to extend the functionality. Discover a growing selection of{" "}
+        <span className="primary-text">AI-Tools</span>,{" "}
+        <span className="primary-text">Hardware-Integrations</span>,{" "}
+        <span className="primary-text">Simulators and More</span>
       </>
     ),
   },
 ];
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <header id="hero" className={`w-full ${styles.heroBackground} h-96`}>
+      <div className="particles absolute w-full h-full">
+        {init && (
+          <Particles
+            id="stars"
+            className="h-full w-full absolute"
+            options={{
+              fullScreen: {
+                enable: false,
+              },
+              fpsLimit: 144,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: false,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "none",
+                    parallax: {
+                      enable: true,
+                      force: 60,
+                      smooth: 10,
+                    },
+                  },
+                },
+                modes: {
+                  push: {
+                    quantity: 1,
+                  },
+                  bubble: {
+                    size: 6,
+                    distance: 10,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#ffffff",
+                },
+                links: {
+                  color: "#ffffff",
+                  distance: 150,
+                  enable: true,
+                  opacity: 0,
+                  width: 1,
+                },
+                move: {
+                  direction: "bottom",
+                  enable: true,
+                  outModes: {
+                    default: "out",
+                  },
+                  size: true,
+                  random: false,
+                  speed: {
+                    min: 0.2,
+                    max: 0.4,
+                  },
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                  },
+                  value: 60,
+                },
+                opacity: {
+                  value: 0.4,
+                },
+                shape: {
+                  type: "polygon",
+                },
+                size: {
+                  value: { min: 1, max: 4 },
+                },
+              },
+              detectRetina: true,
+            }}
+          />
+        )}
+      </div>
+
+      <div className="absolute flex flex-col items-center justify-center w-full h-full">
+        <div className="flex">
+          <div className="flex-col max-w-3xl">
+            <div className="text-center mt-10">
+              <h1 className="font-medium md:text-4xl">
+                The{" "}
+                <span className="primary-text font-bold">
+                  Next Generation IDE
+                </span>{" "}
+                for Electronics Development{" "}
+                <span className="primary-text font-bold">is here</span>.
+              </h1>
+
+              <div className="flex-col md:space-x-5 mb-10">
+                <Link
+                  className="mt-5 button button--primary button--lg hidden md:inline-block"
+                  href="/docs/studio/setup"
+                >
+                  Download
+                </Link>
+
+                <Link
+                  className="mt-5 button button--primary button--outline button--lg"
+                  href="/docs/studio/setup"
+                >
+                  Get Started
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div className={styles.startArrow} />
+    </header>
+  );
+}
 
 export default function Studio(): JSX.Element {
   const slickRef = useRef<Slider>(null);
@@ -63,31 +207,8 @@ export default function Studio(): JSX.Element {
 
   return (
     <Layout title="Studio" description="OneWare Studio IDE">
-      <main className="container mb-20">
-        <div className="text-center mt-10">
-          <h1 className="font-medium">
-            Try the{" "}
-            <span className="primary-text font-bold">Next Generation IDE</span>{" "}
-            for Electronics Development{" "}
-            <span className="primary-text font-bold">now!</span>
-          </h1>
-
-          <div className="flex-col space-x-5 mb-10">
-          <Link
-              className="mt-5 button button--primary button--lg hidden md:inline-block"
-              href="/docs/studio/setup"
-            >
-              Download
-            </Link>
-
-            <Link
-              className="mt-5 button button--primary button--outline button--lg"
-              href="/docs/studio/setup"
-            >
-              Get Started
-            </Link>
-          </div>
-        </div>
+      <HomepageHeader />
+      <main className="container my-20">
         <div style={{ overflowX: "hidden" }}>
           {sliders && sliders.length && (
             <div className="pb-20">
@@ -150,7 +271,7 @@ export default function Studio(): JSX.Element {
 
         <div className="text-center max-w-4xl m-auto">
           <h2 className="text-4xl font-bold">
-          Speedup your Electronics Development with Digital Twins
+            Speedup your Electronics Development with Digital Twins
           </h2>
           <img
             data-aos="zoom-in"
@@ -158,17 +279,22 @@ export default function Studio(): JSX.Element {
             alt="Compare"
           />
           <div className="text-xl md:text-2xl mt-6" data-aos="slide-up">
-            The ONE WARE Studio <span className="primary-text">Code Extension</span> transforms electronics development, 
-            integrating custom electronics with FPGAs and microcontrollers as digital twins directly within our IDE. 
-            Focus on firmware innovation with our seamless <span className="primary-text">Hardware-Firmware Interface</span>, streamline <span className="primary-text">Documentation</span>, 
-            and ease access to past work for future advancements. Tailor the IDE with specific software packages, 
-            optimizing your development process and perfectly matching your project requirements.
+            The ONE WARE Studio{" "}
+            <span className="primary-text">Code Extension</span> transforms
+            electronics development, integrating custom electronics with FPGAs
+            and microcontrollers as digital twins directly within our IDE. Focus
+            on firmware innovation with our seamless{" "}
+            <span className="primary-text">Hardware-Firmware Interface</span>,
+            streamline <span className="primary-text">Documentation</span>, and
+            ease access to past work for future advancements. Tailor the IDE
+            with specific software packages, optimizing your development process
+            and perfectly matching your project requirements.
           </div>
         </div>
 
         <div className="text-center max-w-4xl m-auto mt-32">
           <h2 className="text-4xl font-bold">
-             Integrate AI Faster and Without Experts
+            Integrate AI Faster and Without Experts
           </h2>
           <img
             data-aos="zoom-in"
@@ -176,20 +302,25 @@ export default function Studio(): JSX.Element {
             alt="Compare"
           />
           <div className="text-xl md:text-2xl mt-6" data-aos="slide-up">
-            The ONE WARE Studio <span className="primary-text">AI Extension</span> {" "}
-            is the first solution that moves away from universal
-            yet inefficient neural networks and time consuming tailored AI
-            solutions that depend on AI experts. ONE WARE Studio streamlines AI
-            integration by <span className="primary-text">Guiding Users</span> without prior expertise through the
-            customization of neural networks. It automatically crafts <span className="primary-text">Efficient
-            and Adaptable Neural Networks</span>, which can then be effortlessly
-            deployed across a variety of hardware, from FPGAs to
-            microcontrollers, and processors equipped with TPUs or GPUs. Empower
-            your operations with AI that is both sophisticated and accessible.
+            The ONE WARE Studio{" "}
+            <span className="primary-text">AI Extension</span> is the first
+            solution that moves away from universal yet inefficient neural
+            networks and time consuming tailored AI solutions that depend on AI
+            experts. ONE WARE Studio streamlines AI integration by{" "}
+            <span className="primary-text">Guiding Users</span> without prior
+            expertise through the customization of neural networks. It
+            automatically crafts{" "}
+            <span className="primary-text">
+              Efficient and Adaptable Neural Networks
+            </span>
+            , which can then be effortlessly deployed across a variety of
+            hardware, from FPGAs to microcontrollers, and processors equipped
+            with TPUs or GPUs. Empower your operations with AI that is both
+            sophisticated and accessible.
           </div>
         </div>
 
-        <ContactUs/>
+        <ContactUs />
       </main>
     </Layout>
   );
