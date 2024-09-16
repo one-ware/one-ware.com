b69474eb2ca0177b48e14519794b521bfd0fffd4
Hendrik Mennen <hmennen@protop-solutions.com>
2024-09-16 22:25:48 +0200

    optimization

--- src/pages/one-ai.tsx
+++ src/pages/one-ai.tsx
@@ -97,16 +97,16 @@ function HomepageHeader({ counter }) {
             <ParticlesComponent />
           </div>
           <div className="text-center mt-10">
-            <h1 className="font-medium mb-10 text-6xl" style={{ lineHeight: "1.3" }}>
+            <h1 className="font-medium mb-10 md:text-6xl" style={{ lineHeight: "1.3" }}>
               On a <span className="font-bold">Mission</span> to{" "}
-              <span className="primary-text font-bold text-7xl">Save Trillions</span> Lost to <br />
+              <span className="primary-text font-bold md:text-7xl">Save Trillions</span> Lost to <br />
               <span className="font-bold">Quality Issues</span> and{" "}
               <span className="font-bold">Machine Failures</span>
             </h1>
 
             <h2 className="font-light mb-10 text-5xl" style={{ lineHeight: "1.5" }}>
-              Money Lost Since Opening the Page: <br />
-              <span className="font-bold text-6xl md:text-6xl">
+              Money Lost Since Opening the Page:
+              <span className="font-bold md:text-6xl">
                 ${counter.toLocaleString()}
               </span>
             </h2>
@@ -147,50 +147,51 @@ export default function PLC(): JSX.Element {
 
       <HomepageHeader counter={counter} />
 
-      
-      <main className="container mb-20">   
-        <div className="text-center mt-20" data-aos="slide-up"> 
-        <h2 className="text-5xl font-bold">
-          Your Partner For:
+
+      <main className="container mb-20">
+        <div className="text-center mt-20">
+          <h2 className="text-5xl font-bold" data-aos="slide-up">
+            Your Partner For:
           </h2>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-4" >
-          <div className="text-center" data-aos="slide-up">
-            <img src="/img/ai/quality_small.png" alt="Image 1" className="w-96" />
-            <p className="mt-4 font-bold text-3xl">Quality Control</p>
+          <div className="flex flex-col md:flex-row justify-center items-center gap-4" data-aos="slide-up">
+            <div className="text-center" data-aos="slide-up">
+              <img src="/img/ai/quality_small.png" alt="Image 1" className="w-96" />
+              <p className="mt-4 font-bold text-3xl">Quality Control</p>
+            </div>
+            <div className="text-center" data-aos="slide-up">
+              <img src="/img/ai/root.png" alt="Image 2" className="w-96" />
+              <p className="mt-4 font-bold text-3xl">Root Cause Analysis</p>
+            </div>
+            <div className="text-center" data-aos="slide-up">
+              <img src="/img/ai/predict_small.png" alt="Image 3" className="w-96" />
+              <p className="mt-4 font-bold text-3xl">Predictive Maintenance</p>
+            </div>
           </div>
-          <div className="text-center" data-aos="slide-up">
-            <img src="/img/ai/root.png" alt="Image 2" className="w-96" />
-            <p className="mt-4 font-bold text-3xl">Root Cause Analysis</p>
-          </div>
-          <div className="text-center" data-aos="slide-up">
-            <img src="/img/ai/predict_small.png"  alt="Image 3" className="w-96" />
-            <p className="mt-4 font-bold text-3xl">Predictive Maintenance</p>
-          </div> 
-        </div>
         </div>
 
         <div className="text-center max-w-5xl m-auto mt-20">
           <h2 className="text-5xl font-bold">
-          The Easiest Way to Save Millions per Year
+            The Easiest Way to Save Millions per Year
           </h2>
           <img
             src={"/img/start/saved.png"}
             alt="Compare"
+            className="my-10"
           />
           <div className="text-xl md:text-2xl mt-6">
-          ONE AI, our automated AI developer, can save you millions of dollars annually by <span className="primary-text">detecting quality issues</span>, {" "}
-          <span className="primary-text">identifying root causes</span>, and <span className="primary-text">predicting machine failures</span> before they happen. 
-          The cost of implementing ONE AI is only a <span className="primary-text">small fraction</span> of the potential savings.
+            ONE AI, our automated AI developer, can save you millions of dollars annually by <span className="primary-text">detecting quality issues</span>, {" "}
+            <span className="primary-text">identifying root causes</span>, and <span className="primary-text">predicting machine failures</span> before they happen.
+            The cost of implementing ONE AI is only a <span className="primary-text">small fraction</span> of the potential savings.
           </div>
         </div>
 
         <div className="text-center max-w-5xl m-auto mt-16">
-          <h2 className="text-5xl font-bold">
-          Lowest Risk, Highest Profit
+          <h2 className="text-5xl">
+            Lowest Risk, Highest Profit
           </h2>
 
-          <div className="text-2xl md:text-3xl mt-6">
-          At ONE WARE, you only <span className="primary-text">pay when you're satisfied</span> with the results, not just to see them.
+          <div className="text-2xl md:text-3xl mt-10">
+            At ONE WARE, you only <span className="primary-text">pay when you're satisfied</span> with the results, not just to see them.
           </div>
 
         </div>
@@ -198,7 +199,7 @@ export default function PLC(): JSX.Element {
         <div className="text-center max-w-5xl m-auto mt-16">
 
           <h2 className="text-4xl font-bold">
-          Upfront Cost Comparison
+            Upfront Cost Comparison
           </h2>
 
           <img
@@ -207,8 +208,8 @@ export default function PLC(): JSX.Element {
           />
 
           <div className="text-xl md:text-2xl mt-6">
-          Get in touch with our experts today so we can <span className="primary-text">identify optimization points</span> in your company at no cost, 
-          create impactful <span className="primary-text">return on investment analyses</span> with ONE AI and minimal upfront expenses, and then implement the <span className="primary-text">final system</span>.
+            Get in touch with our experts today so we can <span className="primary-text">identify optimization points</span> in your company at no cost,
+            create impactful <span className="primary-text">return on investment analyses</span> with ONE AI and minimal upfront expenses, and then implement the <span className="primary-text">final system</span>.
           </div>
 
         </div>
@@ -216,7 +217,7 @@ export default function PLC(): JSX.Element {
         <div className="text-center max-w-5xl m-auto mt-16">
 
           <h2 className="text-4xl font-bold">
-          Accuracy and Speed Comparison for PCB Quality Control
+            Accuracy and Speed Comparison for PCB Quality Control
           </h2>
 
           <img
@@ -225,17 +226,17 @@ export default function PLC(): JSX.Element {
           />
 
           <div className="text-xl md:text-2xl mt-6">
-          With ONE AI, you get <span className="primary-text">tailored AI solutions</span> that are not only cheaper. 
-          In our tests, like for industrial PCB defect detection, the AI created by {" "}
-          ONE AI is <span className="primary-text">more accurate</span> and even {" "}
-          <span className="primary-text">multiple times faster</span> than the results of human experts.
-          So you can save money and run your production as fast as possible.
+            With ONE AI, you get <span className="primary-text">tailored AI solutions</span> that are not only cheaper.
+            In our tests, like for industrial PCB defect detection, the AI created by {" "}
+            ONE AI is <span className="primary-text">more accurate</span> and even {" "}
+            <span className="primary-text">multiple times faster</span> than the results of human experts.
+            So you can save money and run your production as fast as possible.
           </div>
-          
+
         </div>
 
         <ContactUs />
-        
+
       </main>
     </Layout>
   );
