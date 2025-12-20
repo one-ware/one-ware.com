a573c8f715ebd54e659f4bd749a6a8d0a673979d
Leon Beier <lbeier@one-ware.com>
2025-12-20 17:42:38 +0100

    Add tracking

--- src/utils/tracking/config.ts
+++ src/utils/tracking/config.ts
@@ -1,12 +1,12 @@
 export const TRACKING_CONFIG = {
   googleAds: {
-    id: "AW-XXXXXXXXXX",
-    conversionId: "AW-XXXXXXXXXX/YYYYYY",
+    id: "AW-17637046883",
+    conversionId: "AW-17637046883/lWpECM7L19QbEOP0_9lB",
   },
   facebook: {
     pixelId: "XXXXXXXXXXXXXXXX",
   },
   reddit: {
-    pixelId: "XXXXXXXX",
+    pixelId: "a2_hssxziyc0jn7",
   },
 };
