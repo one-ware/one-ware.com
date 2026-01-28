223596fc54fb483b8fec423778cb970c1cf378f6
Leon Beier <lbeier@one-ware.com>
2026-01-28 12:09:52 +0100

    remove announcements

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -441,7 +441,7 @@ const config: Config = {
         cta: 'Register Now',
       },
     },
-    activeAnnouncement: 'qualityControlWebinar',
+    // activeAnnouncement: 'qualityControlWebinar',
   } satisfies Preset.ThemeConfig,
 };
 
